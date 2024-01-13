a357debd1ac97bc7f393c307d8abb367bdb0f210
unknown <rahulmorade11@gmail.com>
2024-01-13 20:37:21 +0530

    added json sample data

--- index.js
+++ index.js
@@ -17,6 +17,39 @@ app.get('/about', (req,res) => {
     res.send('<h1>Rahul_Morade</h1>')
 })
 
+app.get('/json', (req,res) => {
+    res.json([
+        {
+            color: "red",
+            value: "#f00"
+        },
+        {
+            color: "green",
+            value: "#0f0"
+        },
+        {
+            color: "blue",
+            value: "#00f"
+        },
+        {
+            color: "cyan",
+            value: "#0ff"
+        },
+        {
+            color: "magenta",
+            value: "#f0f"
+        },
+        {
+            color: "yellow",
+            value: "#ff0"
+        },
+        {
+            color: "black",
+            value: "#000"
+        }
+    ])
+})
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
